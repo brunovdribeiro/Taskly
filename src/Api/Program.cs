55288ca2a6b7812e854044eab20f047a5f205fc0
Bruno Ribeiro <brunovdribeiro@gmail.com>
2025-02-15 20:32:26 +0000

    Add NSwag for API documentation and create task management HTTP requests

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -2,9 +2,14 @@ using Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseOpenApi();
+app.UseSwaggerUi(); 
 
 app.MapTaskEndpoints();
 
