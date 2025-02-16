8da6b948a6167bd066c347a765bd246dd5dfec20
Bruno Ribeiro <brunovdribeiro@gmail.com>
2025-02-16 16:46:38 +0000

    Enhance project configuration; add SPA build target and CORS policy for React app

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -8,7 +8,25 @@ builder.Services.AddOpenApiDocument();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ReactApp", builder =>
+        builder.WithOrigins("http://localhost:3000")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials());
+});
+
 var app = builder.Build();
+
+app.UseCors("ReactApp");
+
+// Serve static files
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapFallbackToFile("index.html");
 
 app.UseOpenApi();
 app.UseSwaggerUi();
